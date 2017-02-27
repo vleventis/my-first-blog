fc7aceec0bb5a46f3702ae0220d372c28e2e2410
Vasilis Leventis <Vasileiosleventis@gmail.com>
2017-02-27 01:20:11 +0000

    My forms app, first commit

--- formsapp/views.py
+++ formsapp/views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from django.contrib.auth.models import AbstractUser
-import django_excel as excel
+
 
 
 @login_required()
